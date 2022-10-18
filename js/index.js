93752077d09570c50e1d81f3540f40a0bbfb92d8
peace <peacebassey30@gmail.com>
2022-10-19 00:05:43 +0100

    change

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-var bat= document.querySelector('.brat')
-
-var fra= document.querySelector('.up')
-var search= document.querySelector('.search')
-var input= document.querySelector('.inmp')
-var my=document.querySelector('.my')
+// var search= document.querySelector('.search')
+// var input= document.querySelector('.inmp')
+// var my=document.querySelector('.my')
 
 
  
-
+var bat= document.querySelector('.brat')
+var fra= document.querySelector('.up')
 bat.addEventListener('click',function(){
+  
 
-fra.classList.add('disp')
+fra.classList.add("dis");
+alert('hi')
 
 
 
--- js/shoe.js
+++ js/shoe.js
@@ -1,18 +1,15 @@
 var buy= document.querySelectorAll('.buy')
 
-var ca= document.querySelector('.ca')
-
-var quantity= document.querySelector('.quantity')
+var ca= document.querySelector('h1')
 
 buy.forEach((b)=>{
 
 b.addEventListener('click',function(){
 
-    
+    var add= Number(ca.getAttribute('data-count')|| 0);
+    ca.setAttribute('data-count', add + 1);
+    ca.classList.add('zero')
 
-    quantity.textContent=+1
-
- 
 
 })
 
